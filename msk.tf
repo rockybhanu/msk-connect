7a8f277a37fc6b40c2a502d58225cad63c560c8e
rockybhanu <rampooja245@gmail.com>
2024-09-01 00:05:44 +0530

    msk update ips

--- msk.tf
+++ msk.tf
@@ -1,23 +1,23 @@
 resource "aws_msk_cluster" "ramanuj-dev" {
   cluster_name           = "ramanuj-dev-cluster"
   kafka_version          = "3.6.0"
-  number_of_broker_nodes = 2  # Two nodes for high availability
+  number_of_broker_nodes = 2 # Two nodes for high availability
 
   broker_node_group_info {
-    instance_type = "kafka.t3.small"  # Cheapest instance type for testing
+    instance_type = "kafka.t3.small" # Cheapest instance type for testing
 
-    client_subnets  = [aws_subnet.msk_subnet[0].id, aws_subnet.msk_subnet[1].id]  # Use two subnets
+    client_subnets  = [aws_subnet.msk_subnet[0].id, aws_subnet.msk_subnet[1].id] # Use two subnets
     security_groups = [aws_security_group.msk_security_group.id]
 
     storage_info {
       ebs_storage_info {
-        volume_size = 100  # Minimal storage for testing
+        volume_size = 100 # Minimal storage for testing
       }
     }
 
     connectivity_info {
       public_access {
-        type = "SERVICE_PROVIDED_EIPS"  # This enables public access to the brokers
+        type = "SERVICE_PROVIDED_EIPS" # This enables public access to the brokers
       }
     }
   }
